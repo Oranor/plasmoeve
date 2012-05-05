2e9ac91db6fde26db29d69d19f93c2c97f94b88a
Rolf Herzog <her@gmx.de>
2012-05-05 10:19:09 +0200

    added tousand separator, total skillpoints and queue progress

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ plasmoeve
 =========
 
 KDE Plasma Applet for displaying Eve character information
+
+This is a place to store my enhancements to the KDE Plasma app from Iman Karim.
+See http://kde-apps.org/content/show.php?action=content&content=146516 for the original app.
+
+This original version is tagged as "plasmoeve-0.1"
+
+ChangeLog
+---------
+
+### plasmoeve-0.1
+ * Original version from Imam Karim
+
+### plasmoeve-0.2
+ * added thousand separator for large numbers
+ * added display of total skill points
+ * now displays progress of the whole skill queue instead of only the current skill in training
--- contents/code/main.py
+++ contents/code/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # by Iman Karim (imax@tha-imax.de)
+# contributed: Rolf Herzog <her@gmx.de>
 # Released under GPLv2
 
 from PyQt4.QtCore import *
@@ -13,26 +14,33 @@ import socket
 import eveapi
 import eveConfig
 import ConfigParser
- 
+import locale
+
 class EveApplet(plasmascript.Applet):
     APIKEY = None
     USERID = None
     CHAR   = None
     timer  = None
+    qtimer = None
     timeResult = None
+    queueTimeEnd = None
+    skillTree = None
     timerruns = 0
+    qtimerruns = 0
     html_header = """<html>
     """
-    
 
-    
+
+
     def __init__(self,parent,args=None):
         plasmascript.Applet.__init__(self,parent)
         self.imagelabel = Plasma.Label(self.applet)
         self.timelabel = Plasma.Label(self.applet)
         self.namelabel = Plasma.Label(self.applet)
+        self.skillPointLabel = Plasma.Label(self.applet)
         self.isklabel = Plasma.Label(self.applet)
         self.skilllabel = Plasma.Label(self.applet)
+        self.queueTimeLabel = Plasma.Label(self.applet)
         self.char  = None
         self.skillCache = None
 
@@ -44,17 +52,24 @@ class EveApplet(plasmascript.Applet):
             self.APIKEY = self.config.get("api", "apikey")
             self.USERID = self.config.get("api", "userid")
             self.CHAR = self.config.get("api", "char")
-            
+
+        self.setAspectRatioMode(0)
         self.setHasConfigurationInterface(True)
-        self.resize(300, 300)
+        self.resize(350, 300)
         self.layout = QGraphicsLinearLayout(Qt.Vertical, self.applet)
+        self.skilllabel.setWordWrap(False)
+        self.imagelabel.setAlignment(Qt.AlignHCenter)
         self.layout.addItem(self.imagelabel)
         self.layout.addItem(self.namelabel)
+        self.layout.addItem(self.skillPointLabel)
         self.layout.addItem(self.isklabel)
         self.layout.addItem(self.skilllabel)
         self.layout.addItem(self.timelabel)
+        self.layout.addItem(self.queueTimeLabel)
+        self.layout.setStretchFactor(self.skilllabel, 4)
         self.imagelabel.setText("Refreshing...")
         self.namelabel.setText("Refreshing...")
+        self.skillPointLabel.setText("Refreshing...")
         self.isklabel.setText("Refreshing...")
         self.skilllabel.setText("Refreshing...")
         self.refresh()
@@ -73,8 +88,8 @@ class EveApplet(plasmascript.Applet):
         found = False
         try:
             result = self.api.account.Characters(keyID=self.USERID, vCode=self.APIKEY)
-            
-            
+
+
             for c in result.characters:
                 if (c.name == self.CHAR):
                     self.char = c
@@ -82,7 +97,7 @@ class EveApplet(plasmascript.Applet):
                     break
         except:
             pass
-        
+
         return(found)
 
     def getImageURL(self):
@@ -92,12 +107,19 @@ class EveApplet(plasmascript.Applet):
         result = self.api.char.SkillInTraining(keyID=self.USERID, vCode=self.APIKEY, characterID=self.char.characterID)
         return(result)
 
+    def formatNumber(self, number):
+        (lang, encoding) = locale.getdefaultlocale()
+        locale.setlocale(locale.LC_ALL, lang)
+        return locale.format("%d", number, grouping=True)
+
     def getISK(self):
         isk = 0
+        (lang, encoding) = locale.getdefaultlocale()
+        locale.setlocale(locale.LC_ALL, lang)
         try:
             auth = self.api.auth(keyID=self.USERID, vCode=self.APIKEY)
             wallet = auth.char.AccountBalance(characterID=self.char.characterID)
-            isk = str(wallet.accounts[0].balance) + " ISK"
+            isk = locale.format("%d", wallet.accounts[0].balance, grouping=True) + " ISK"
         except:
             isk = "<html><font color='red'>Failed. Low API Level?"
         return(isk)
@@ -105,53 +127,67 @@ class EveApplet(plasmascript.Applet):
     def refresh(self):
         self.api = eveapi.EVEAPIConnection()
         found = self.checkUser()
-        
-        
-        
+
         if (not found):
             self.imagelabel.setText("<html>No character configured!<br>Please open up the configuration and setup your char.")
             return
-        
-        
         if (not self.skillCache):
                 self.skillCache = self.api.eve.SkillTree(keyID=self.USERID, vCode=self.APIKEY)
-        
         imageURL = self.getImageURL()
         ret = self.request(imageURL)
         localImage = "/tmp/.plasmoeve_portrait_" + os.getenv('USER')  + "_" + self.char.name + ".jpg"
         f = open(localImage, "w")
         f.write(ret)
         f.close()
-        
+
         self.timeResult = self.getSkillInTraining()
         self.timerruns = 0
         if (self.timer == None):
             self.timer = QTimer()
             self.connect(self.timer, SIGNAL("timeout()"), self.refreshTime)
         self.timer.start(1000)
-            
+
+        self.retrieveSkillQueue()
+        self.qtimerruns = 0
+        if (self.qtimer == None):
+            self.qtimer = QTimer()
+            self.connect(self.timer, SIGNAL("timeout()"), self.refreshQueueTime)
+        self.qtimer.start(1000)
+
+        skillName = self.getSkillName(self.timeResult.trainingTypeID)
+        skillLevel = self.timeResult.trainingToLevel
+        isk = self.getISK()
+        skillPoints = self.formatNumber(self.getSkillPoints())
+
+        self.imagelabel.setText("<html><img src='%s'>" %(localImage))
+        self.namelabel.setText("<html><table><tr><td width='128'><b>Pilot:</td><td>%s</b>" %(self.char.name))
+        self.skillPointLabel.setText("<html><table><tr><td width='128'><b>Skillpoints:</td><td>%s</b>" %(skillPoints))
+        self.isklabel.setText("<html><table><tr><td width='128'><b>Wealth:</td><td>%s" %(isk))
+        self.skilllabel.setText("<html><table><tr><td width='128'><b>Skilling:</td><td>%s (L: %d)" %(skillName, skillLevel))
+
+    def getSkillName(self, skillId):
+        if (not self.skillCache):
+                self.skillCache = self.api.eve.SkillTree(keyID=self.USERID, vCode=self.APIKEY)
         skillName = "<font color='red'>Not skilling!</font>"
-        skillLevel = 0
         try:
             for skillGroup in self.skillCache.skillGroups:
                 for skill in skillGroup.skills:
-                    if (skill.typeID == self.timeResult.trainingTypeID):
+                    if (skill.typeID == skillId):
                         skillName = skill.typeName
-                        skillLevel = self.timeResult.trainingToLevel
                         break
         except:
             pass
-        
-        isk = self.getISK()
-        
-        self.imagelabel.setText("<html><img src='%s'>" %(localImage))
-        self.namelabel.setText("<html><table><tr><td width='128'><b>Pilot:</td><td>%s</b>" %(self.char.name))
-        self.isklabel.setText("<html><table><tr><td width='128'><b>Wealth:</td><td>%s" %(isk))
-        self.skilllabel.setText("<html><table><tr><td width='128'><b>Skilling:</td><td>%s (L: %d)" %(skillName, skillLevel))
-        
-        
+        return skillName
+
+    def getSkillPoints(self):
+        characterSheet = self.api.char.CharacterSheet(keyID=self.USERID, vCode=self.APIKEY, characterID=self.char.characterID)
+        skillRowset = characterSheet.skills
+        totalSP = 0
+        for skill in skillRowset:
+            totalSP += skill.skillpoints
+        return totalSP
+
     def refreshTime(self):
-        
         if self.timeResult.skillInTraining:
             timeLeft = self.timeResult.trainingEndTime - self.timeResult._meta.currentTime  - (1000 + self.timerruns)
             if timeLeft < 0:
@@ -159,14 +195,7 @@ class EveApplet(plasmascript.Applet):
                 timeLeft = -timeLeft
             else:
                 complete = False
-    
-            # make a nice formatted time string
-            s = timeLeft % 60
-            m = (timeLeft/60) % 60
-            h = (timeLeft/3600) % 24
-            d = (timeLeft/86400)
-            timeString = "%2dd %2dh %2dm %2ds" % (d, h, m, s)
-    
+            timeString = self.formatTime(timeLeft)
             msg = ""
             if complete:
                 msg = "<font color='green'>TRAINING COMPLETE (%s ago)</font>" % (timeString)
@@ -174,16 +203,57 @@ class EveApplet(plasmascript.Applet):
                 return
             else:
                 msg = "%s" % (timeString)
-            
+
             timelbl = "<html><table><tr><td width='128'><b>Time remaining:</b></td>"
             timelbl += "<td>%s</td>" %(msg)
             timelbl += "</tr>"
             self.timelabel.setText(timelbl)
-            
+
             self.timerruns += 1
 
-            
-    
+    def refreshQueueTime(self):
+        if self.skillTree:
+            timeLeft = self.queueTimeEnd - self.skillTree._meta.currentTime  - (1000 + self.qtimerruns)
+            if timeLeft < 0:
+                complete = True
+                timeLeft = -timeLeft
+            else:
+                complete = False
+
+            timeString = self.formatTime(timeLeft)
+
+            msg = ""
+            if complete:
+                msg = "<font color='green'>TRAINING COMPLETE (%s ago)</font>" % (timeString)
+                self.refresh()
+                return
+            else:
+                msg = "%s" % (timeString)
+
+            timelbl = "<html><table><tr><td width='128'><b>Queue remaining:</b></td>"
+            timelbl += "<td>%s</td>" %(msg)
+            timelbl += "</tr>"
+            self.queueTimeLabel.setText(timelbl)
+
+            self.qtimerruns += 1
+
+    def retrieveSkillQueue(self):
+        self.queueEndTime = 0
+        self.skillTree = self.api.char.SkillQueue(keyID=self.USERID, vCode=self.APIKEY, characterID=self.char.characterID)
+        rowSet = self.skillTree.skillqueue
+        rowSet.SortBy("queuePosition")
+        for queueEntry in rowSet:
+            self.queueTimeEnd = queueEntry.endTime
+
+    def formatTime(self,rawTime):
+        """make a nice formatted time string"""
+        s = rawTime % 60
+        m = (rawTime/60) % 60
+        h = (rawTime/3600) % 24
+        d = (rawTime/86400)
+        timeString = "%2dd %2dh %2dm %2ds" % (d, h, m, s)
+        return timeString
+
     def createConfigurationInterface(self,parent):
         self.eveConfig = eveConfig.eveConfig(self)
         page = parent.addPage(self.eveConfig,"Eve Char Selection")
@@ -225,6 +295,6 @@ class EveApplet(plasmascript.Applet):
             if (self.USERID != None): self.eveConfig.txtUserID.setText(self.USERID)
             dialog.resize(400,400)
             dialog.exec_()
-        
+
 def CreateApplet(parent):
     return EveApplet(parent)
